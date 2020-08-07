4fb24c63f5c907d91cd157e3ba0507d9c2f18ccd
divi tegar hidayahtulloh <55317365+divitegar@users.noreply.github.com>
2020-08-07 13:06:31 +0700

    add todoform function

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-const TodoForm = () => {
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const HandleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!value) {
+      alert("No blank Todo!");
+      return;
+    }
+
+    addTodo(value);
+    alert(value);
+    setValue("");
+  };
   return (
     <section className="add">
-      <form className="add-form">
-        <input type="text" className="add-input" />
+      <form className="add-form" onSubmit={HandleFormSubmit}>
+        <input
+          type="text"
+          className="add-input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <button className="add-btn main-black-color">add</button>
       </form>
     </section>
   );
+};
+
+TodoForm.propTypes = {
+  addTodo: PropTypes.func.isRequired
 };
 
 export default TodoForm;
