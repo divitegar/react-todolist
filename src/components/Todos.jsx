baf99fb02f9121fc8d3245a9e3024a2fd340e7f9
divi tegar hidayahtulloh <55317365+divitegar@users.noreply.github.com>
2020-08-07 11:19:00 +0700

    add todolist layout

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,23 +1,32 @@
 import React from "react";
+import Todo from "./Todo";
 
 const Todos = () => {
+  const todos = [
+    {
+      text: "Belajar React1"
+    },
+    {
+      text: "Belajar React2"
+    },
+    {
+      text: "Belajar React3"
+    },
+    {
+      text: "Belajar React4"
+    },
+    {
+      text: "Belajar React5"
+    },
+    {
+      text: "Belajar React6"
+    }
+  ];
   return (
     <section className="todos">
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
+      {todos.map((todo) => {
+        return <Todo text={todo.text} />;
+      })}
     </section>
   );
 };
