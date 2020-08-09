c0ee735413d7ad4b5a78b8d7f01855e0044fedc9
divi tegar hidayahtulloh <55317365+divitegar@users.noreply.github.com>
2020-08-09 11:51:13 +0700

    only 10 todos allowed

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -15,9 +15,13 @@ const TodoList = () => {
   const [showAdd, setShowAdd] = useState(false);
 
   const addTodo = (value) => {
-    const addedTodo = [...todos, { text: value, isCompleted: false }];
+    if (todos.length < 10) {
+      const addedTodo = [...todos, { text: value, isCompleted: false }];
 
-    setTodos(addedTodo);
+      setTodos(addedTodo);
+    } else {
+      alert("only 10 todos is allowed!");
+    }
   };
 
   const completeTodo = (index) => {
@@ -26,8 +30,6 @@ const TodoList = () => {
 
     setTodos(addedTodo);
   };
-
-  console.log("todos", todos);
 
   const showAddToggle = () => setShowAdd(!showAdd);
 
