b062323738b8da111613d60d46de7b0ff6bd85ea
divi tegar hidayahtulloh <55317365+divitegar@users.noreply.github.com>
2020-08-16 21:46:50 +0700

    class component paper

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 import PropTypes from "prop-types";
-import Button from "../button/Button";
-import styles from "./header.module.css";
+import Button from "../button/ButtonClass";
+// import styles from "./header.module.css";
+import * as styles from "./header.styles";
+import { useTheme } from "emotion-theming";
+import Container from "../../layout/Container";
+import Item from "../../layout/Item";
 
 const Header = ({ showAddToggle, showAdd, clearTodos }) => {
+  const theme = useTheme();
+
   return (
-    <section className={styles.header}>
-      <Button text={showAdd ? "finish" : "add"} onClick={showAddToggle} />
-      <h1 className={styles.headerTitle}>Todo Lists</h1>
-      <Button text="Clear" onClick={clearTodos} color="red" align="right" />
+    <section className="header-component">
+      <Container alignItems="flex-start">
+        <Item flex={1}>
+          <Button text={showAdd ? "finish" : "add"} onClick={showAddToggle} />
+        </Item>
+        <Item flex={2}>
+          <h1 css={styles.headerTitle(theme)}>Todo Lists</h1>
+        </Item>
+        <Item flex={1} align="right">
+          <Button text="Clear" onClick={clearTodos} color="red" align="right" />
+        </Item>
+      </Container>
     </section>
   );
 };
