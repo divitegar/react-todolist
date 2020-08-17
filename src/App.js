2fed09332351a24ce56ebcfd092accee23b1f026
divi tegar hidayahtulloh <55317365+divitegar@users.noreply.github.com>
2020-08-17 14:29:54 +0700

    add todos class and todolist class

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles.css";
 import { ThemeProvider } from "emotion-theming";
-import TodoList from "./pages/TodoList";
+import TodoList from "./pages/TodoListClass";
 
 const theme = {
   color: {
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 
 import Paper from "../components/paper/PaperClass";
 import Header from "../components/header/HeaderClass";
-import TodoForm from "../components/todoform/TodoForm";
-import Todos from "../components/todos/Todos";
+import TodoForm from "../components/todoform/TodoFormClass";
+import Todos from "../components/todos/TodosClass";
 import Container from "../layout/Container";
 
 const TodoList = () => {
