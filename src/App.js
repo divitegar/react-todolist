7c8ce3b4b0e97d7a4f6b5350b69b9b841b7f00ef
divi tegar hidayahtulloh <55317365+divitegar@users.noreply.github.com>
2020-08-17 14:51:33 +0700

    add localstorage todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles.css";
 import { ThemeProvider } from "emotion-theming";
-import TodoList from "./pages/TodoListClass";
+import TodoList from "./pages/TodoList";
 
 const theme = {
   color: {
